a9e490e2c5bfbbde027b216c41197e8d402152bf
vrunoa <vruno.alassia@gmail.com>
2019-03-24 16:49:54 -0700

    adding a base android app

---
+++ android-app/app/src/main/java/io/appium/appiumworkshop/MainActivity.java
@@ -0,0 +1,14 @@
+package io.appium.appiumworkshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
