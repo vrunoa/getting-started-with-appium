083d178c57a537c2b84858f8a114d362726ea5d2
vrunoa <vruno.alassia@gmail.com>
2019-03-24 19:28:14 -0700

    update android app, add new activity

---
+++ android-app/app/src/main/java/io/appium/appiumworkshop/LoginActivity.java
@@ -0,0 +1,13 @@
+package io.appium.appiumworkshop;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
