b639c0e0956ee297c3c94822b2cef14917b29bb7
vrunoa <vruno.alassia@gmail.com>
2019-03-24 21:25:15 -0700

    update app icon, update apk

--- test/test_android.js
+++ test/test_android.js
@@ -28,7 +28,6 @@ if (process.env.CLOUD_PROVIDER) {
         app_path: app
     })
     if (err) {
-        console.log(err);
         throw new Error("Failed to upload app! :(");
     }
     caps['app'] = 'sauce-storage:app-debug.apk'
