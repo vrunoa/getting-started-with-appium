af0c0969aa715653495137ee84f20ac89fa7e1fd
vrunoa <vruno.alassia@gmail.com>
2019-03-24 21:11:44 -0700

    add some logs

--- test/test_android.js
+++ test/test_android.js
@@ -28,6 +28,7 @@ if (process.env.CLOUD_PROVIDER) {
         app_path: app
     })
     if (err) {
+        console.log(err);
         throw new Error("Failed to upload app! :(");
     }
     caps['app'] = 'sauce-storage:app-debug.apk'
