ccc52578206251e6e8b34e01a9c740207465adb5
vrunoa <vruno.alassia@gmail.com>
2019-03-31 20:06:29 -0700

    add system app test example

---
+++ test/test_android_calculator.js
@@ -0,0 +1,40 @@
+const wd = require('wd');
+const sleep = require('asyncbox').sleep
+const chai = require('chai');
+
+const expect = chai.expect
+let driver, res;
+let endpoint = 'http://localhost:4723/wd/hub'
+
+let caps = {
+    'appPackage': 'com.android.calculator2',
+    'appActivity': 'com.android.calculator2.Calculator',
+    'appWaitActivity': 'com.android.calculator2.Calculator',
+    'deviceName': 'Android GoogleApi Emulator',
+    'platformName': 'Android',
+    'platformVersion': '8.0'
+}
+
+describe('Android Workshop calculator tests', async () => {
+    before(async () => {
+        driver = await wd.promiseChainRemote(endpoint)
+        res = await driver.init(caps)
+        console.log(`Session ID ${res[0]}`);
+    });
+    after(async () => {
+        await driver.quit()
+    });
+    it('Test calculator can multiply', async () => {
+        console.log(await driver.source())
+        let el = await driver.elementById('com.android.calculator2:id/digit_2');
+        await el.click();
+        el = await driver.elementById('com.android.calculator2:id/op_mul');
+        await el.click();
+        el = await driver.elementById('com.android.calculator2:id/digit_3');
+        await el.click();
+        el = await driver.elementById('com.android.calculator2:id/eq');
+        await el.click()
+        el = await driver.elementById('com.android.calculator2:id/result');
+        expect(await el.text()).to.equal('6');
+    });
+});
